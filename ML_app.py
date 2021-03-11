c2e159edd899a50d5ad9f3f7e55157974a33c46f
hoon1016 <wjsrms76@gmail.com>
2021-03-11 11:57:31 +0900

    버전1.0

--- ML_app.py
+++ ML_app.py
@@ -17,25 +17,47 @@ import joblib
 def run_ML_app():
     st.subheader('Maching Learnig')
 
-    model = tensorflow.keras.models.load_model('data/car_ai.h5')
     
-    new_data = np.array([0,38,90000,2000,500000])
 
-    new_data = new_data.reshape(1,-1)
+    #1.유저한테 입력을 받는다.
+    #성별
+    gendar = st.radio("성별을 선택하세요",['남자','여자'])
+    if gendar == '남자':
+        gendar = 1
+    else:
+        gendar = 0
     
+    age = st.number_input("나이 입력",min_value=0,max_value=120)
+    
+    salary = st.number_input('연봉 입력',min_value=0)
+
+    debt = st.number_input('빚 입력',min_value=0)
+
+    worth = st.number_input('자산 입력',min_value=0)
+
+    # 2. 예측한다.
+    # 2- 1. 모델 불러오기
+    model = tensorflow.keras.models.load_model('data/car_ai.h5')
+    #2-2. 넘파이 어레이 만든다.
+
+    new_data = np.array([gendar,age,salary,debt,worth])
+    #2-3. 피쳐스케일링하자.
+    new_data = new_data.reshape(1,-1)
     sc_X = joblib.load('data/sc_X.pkl')
     
     new_data = sc_X.transform(new_data)
-
+    #2-4. 예측한다.
     y_pred=model.predict(new_data)
-
+    # 예측 결과는, 스케일링 된 결과이므로, 다시 돌려야한다.
     #st.write(y_pred[0][0])
-
     sc_y = joblib.load('data/sc_y.pkl')
-
     orginal = sc_y.inverse_transform(y_pred)
-
-    st.write(orginal[0,0])
+    
+    # 3. 결과를 화면에 보여준다.
+    bth = st.button('결과 보기')
+    if bth :
+        st.write('예측 결과입니다. {:,.1f} 달러의 차를 살수 있습니다'.format(orginal[0,0],))
+    
 
 
 
--- eda_app.py
+++ eda_app.py
@@ -25,3 +25,49 @@ def run_eda_app():
     elif selected_radio == '통계치':
         st.dataframe(car_df.describe())
     
+    columns = car_df.columns
+    columns = list(columns)
+    
+    selected_columns = st.multiselect('컬럼을 선택하시오',columns)
+    if len(selected_columns) != 0 :
+        st.dataframe(car_df[selected_columns])
+    else:
+        st.write('선택한 컬럼이 없습니다')
+    # 상관 계수를 화면에 보여주도록 만듭니다.
+    # 멀티셀럭트에 컬럼명을 보여주고.
+    #해당 컬럼들에 대한 상관계수를 보여주세요
+    #단,컬럼들은 숫자 컬럼들만 멀티셀럭트에 나타나야합니다
+
+    print(car_df.dtypes == object)
+
+    corr_columns = car_df.columns[car_df.dtypes != object]
+    corr_list = st.multiselect('상관 계수를 볼 컬럼을 선택하세요',corr_columns)
+    
+    if len(corr_list) != 0 :
+        st.dataframe(car_df[corr_list].corr())
+    #위에서 선택한 컬럼들을 이용해서, 시본의 페어플롯을
+    #그린다
+        st.pyplot(sns.pairplot(car_df[corr_list]))
+    
+    else:
+        st.write('선택한 컬럼이 없습니다')
+    
+    #컬럼을 하나만 선택하면, 해당 컬럼의 min과 max에 
+    #해당하는 사람의 데이터를 화면에 보여주는 기능
+    menu = corr_columns
+    choice = st.selectbox('Min & Max',menu)
+    
+    min_data = car_df[choice].min() == car_df[choice]
+    st.write('최소값 데이터')
+    st.dataframe(car_df.loc[min_data,])
+    
+    max_data = car_df[choice].max() == car_df[choice]
+    st.write('최대값 데이터')
+    st.dataframe(car_df.loc[max_data,])
+    #고객이름을 검색할 수 있는 기능 개발    
+    name = st.text_input('고객의 이름을 입력하세요')
+    search_name=car_df.loc[car_df['Customer Name'].str.contains(name,case = False),]
+    if len(search_name) != 0:
+        st.dataframe(search_name)
+    else:
+        st.write('해당 고객에 정보가 없습니다')
