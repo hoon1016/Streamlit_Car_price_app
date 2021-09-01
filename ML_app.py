3fc0f5e97332f96eeafe8fce5b305875107dbfde
hoon1016 <80316237+hoon1016@users.noreply.github.com>
2021-09-02 02:51:46 +0900

    fix

--- ML_app.py
+++ ML_app.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 import tensorflow.keras
-from keras.models import Sequential
-from keras.layers import Dense,Dropout
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense,Dropout
 from sklearn.preprocessing import MinMaxScaler
 import h5py
 from tensorflow.keras.callbacks import ModelCheckpoint,CSVLogger
